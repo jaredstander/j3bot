91b5518bf3b852ecb2442bcd4086555a44b35777
jaredstander <jaredstander@gmail.com>
2014-04-11 22:19:48 -0400

    Adding console output to help identify admin permissions problem.

--- helpers/check_user.rb
+++ helpers/check_user.rb
@@ -2,11 +2,14 @@ module Cinch
   module Helpers
 
     def admin?(user, admin_list)
-      p " \n "
-      p user.mask
-      p user.nick
-      p user.mask.to_s.slice!((user.nick.length + 1), user.mask.to_s.length)
-      p " \n "
+      puts " "
+      puts " "
+      puts admin_list
+      puts user.mask
+      puts user.nick
+      puts user.mask.to_s.slice!((user.nick.length + 1), user.mask.to_s.length)
+      puts " "
+      puts " "
       admin_list.include?(user.mask.to_s.slice!((user.nick.length + 1), user.mask.to_s.length))
     end
 
